ae28bdbfcd26949615ccc6abbc90a0a3d161c99b
Leonardo Guarilha <lsguarilha01@gmail.com>
2020-08-07 15:22:03 -0300

    Finish v1

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import Landing from "./src/pages/Landing";
+import AppStack from "./src/routes/AppStack";
 
 export default function App() {
   return (
     <>
-      <Landing />
-      <StatusBar style="auto" />
+      <AppStack />
+      <StatusBar style="light" />
     </>
   );
 }
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -23,6 +23,41 @@ const styles = StyleSheet.create({
   titleBold: {
     fontWeight: "bold",
   },
+
+  buttonsContainer: {
+    flexDirection: "row",
+    marginTop: 40,
+    justifyContent: "space-between",
+  },
+
+  button: {
+    height: 150,
+    width: "48%",
+    borderRadius: 8,
+    padding: 24,
+    justifyContent: "space-between",
+  },
+
+  buttonPrimary: {
+    backgroundColor: "#9871f5",
+  },
+
+  buttonSecondary: {
+    backgroundColor: "#04d361",
+  },
+
+  buttonText: {
+    color: "#fff",
+    fontSize: 20,
+  },
+
+  totalConnections: {
+    color: "#d4c2ff",
+    fontSize: 12,
+    lineHeight: 20,
+    maxWidth: 140,
+    marginTop: 40,
+  },
 });
 
 export default styles;
